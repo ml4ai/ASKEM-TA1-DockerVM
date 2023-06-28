917af0ecfa9ba87902fa2daf1b5e2ba8783a6a0a
Enrique Noriega <enoriega@arizona.edu>
2023-06-28 13:22:08 -0700

    TextReading Integration Notebook (#15)
    * Added TR integration notebook
    
    * Removed the OPENAI_KEY env from the notebook
    
    * Point  to the latest image
    
    * Updated notebook to be compatible with python 3.8
    
    * Verified that the TR notebook runs within the container's jupyter server
    
    * Updates to README
    
    ---------
    
    Co-authored-by: Gus Hahn-Powell <ghp@lum.ai>

--- legacy/client.sh
+++ legacy/client.sh
@@ -3,10 +3,10 @@
 while read FILE
 do
 	NAME=$(basename $FILE)
-	OUTPUT="/data/outputs/mit_$NAME"
+	OUTPUT="./outputs/mit_$NAME"
 	curl -X 'POST' \
-		"http://mit:8000/annotation/upload_file_extract/?gpt_key=$OPENAI_KEY" \
+		"http://localhost:8000/annotation/upload_file_extract/?gpt_key=$OPENAI_KEY" \
 		-H 'accept: application/json' \
 		-H 'Content-Type: multipart/form-data' \
 		-F "file=@$FILE;type=text/plain" > "$OUTPUT"
-done < <(find /data/inputs -type f)
+done < <(find ./inputs -type f)
